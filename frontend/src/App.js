d6748f71d4318df158bb1e4cc5ae48a640ed75d2
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-24 06:37:58 -0300

    Finalizado primeira tela do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="Logo do Projeto" />
+
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ofereça <strong>spots</strong> para progrmadores e encontre <strong>talentos</strong> para sua empresa!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
