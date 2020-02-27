818bebccf7e7a876f6a340e4011c79185d7a8d99
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-27 09:27:15 -0300

    Primeira tela pronta Login

--- mobile/src/page/Login.js
+++ mobile/src/page/Login.js
@@ -1,8 +1,82 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+
+import logo from '../assets/logo.png'
 
 export default function Login () {
-  return <View>
-    <Text>Olá Mundo!</Text>
-  </View>
+  return( 
+  <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
+    <Image source={logo} />
+
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu E-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tecnologias"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Encontrar spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#444',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    marginBottom: 20,
+    height: 44,
+    borderRadius: 2
+  },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
