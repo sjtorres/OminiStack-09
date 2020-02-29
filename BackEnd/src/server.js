d1a89c9a68f17d6a65be26c7e698ce078a61735c
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-29 07:15:18 -0300

    Configurando socket.io

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -3,13 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http')
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://oministack:oministack@oministack9-wuril.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+
+const connecteUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connecteUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connecteUsers = connecteUsers;
+
+  return next();
 })
 
 // GET, POST, PUT, DELETE
@@ -22,4 +42,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './style.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user');
+    const socket = socketio('http://localhost:3333');
+
+    socket.emit('omni', 'Stack');
+  }, []);
 
   useEffect(() => {
     async function loadSpots() {
